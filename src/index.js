a3cb2c1606134d12d900f8707499c7cac203e282
Melina Estefanía <meapilon@gmail.com>
2021-09-26 00:34:38 -0300

    LocalStorage

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from "./store"
+import { store, persistor } from "./store"
 import App from './App';
+import {PersistGate} from 'redux-persist/integration/react'
 
 ReactDOM.render(
   <Provider store={store}>
+     <PersistGate loading={null} persistor={persistor}>
     <App />
+    </PersistGate>
     </Provider>,
   document.getElementById('root')
 );
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,21 @@
-import { createStore } from "redux";
-import reducer from "../Reducer";
+import { createStore } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist' // imports from redux-persist
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+
+import reducer from '../Reducer/index' // Root reducer
+
+const persistConfig = { // configuration object for redux-persist
+    key: 'root',
+    storage, // define which storage to use
+}
+
+const persistedReducer = persistReducer(persistConfig, reducer) // create a persisted reducer
 
 const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+  persistedReducer, // pass the persisted reducer instead of rootReducer to createStore
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // add any middlewares here
+)
 
-export default store;
+const  persistor = persistStore(store); // used to create the persisted store, persistor will be used in the next step
+
+export {store, persistor}
